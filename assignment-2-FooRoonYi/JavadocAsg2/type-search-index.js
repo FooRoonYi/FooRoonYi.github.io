c1f55e0c522c47d33c1f4b80f0a14a837d249831
Foo Roon Yi <80664992+FooRoonYi@users.noreply.github.com>
2022-12-17 13:48:58 +0800

    Delete assignment-2-FooRoonYi directory

--- assignment-2-FooRoonYi/JavadocAsg2/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"my.uum","l":"DatabaseManager"},{"p":"my.uum","l":"Main"},{"p":"my.uum","l":"MyBot"}];updateSearchResults();
